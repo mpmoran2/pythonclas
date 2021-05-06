8d1894b1e638910ba8ef182dbea10477e654cb3f
mpmoran2 <kamokumoran@gmail.com>
2021-05-06 16:01:59 -0700

    Database connected with psycopg2

--- demo.py
+++ demo.py
@@ -1,2 +1,6 @@
-print('Hi ho')
-print('We set up more stuff yo')
+import psycopg2
+
+#  connect to a database. go to psql and get the info you need
+conn = psycopg2.connect(dbname="postgres", user="postgres", password="2349", host="localhost", port="5432")
+
+print("Database Connection Successful")
