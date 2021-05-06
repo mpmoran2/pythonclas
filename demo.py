a2394565673e600101a9b12cc6f22326cb64e3cc
mpmoran2 <kamokumoran@gmail.com>
2021-05-06 16:08:45 -0700

    Created table in our database

--- demo.py
+++ demo.py
@@ -2,5 +2,11 @@ import psycopg2
 
 #  connect to a database. go to psql and get the info you need
 conn = psycopg2.connect(dbname="postgres", user="postgres", password="2349", host="localhost", port="5432")
+cur = conn.cursor()
+# once cursor is created, to create a table
+cur.execute('''CREATE TABLE demo(ID serial, Name text, Color text, Age int);''')
+print("Table Created")
+# to save it
+conn.commit()
+conn.close()
 
-print("Database Connection Successful")
