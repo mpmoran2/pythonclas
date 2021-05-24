4b3d2423259a9cf01ead47017c837864be232e6b
mpmoran2 <kamokumoran@gmail.com>
2021-05-24 12:54:55 -0700

    Save entries to Database

--- sheep.py
+++ sheep.py
@@ -1,7 +1,17 @@
 from tkinter import *
 import tkinter as tk
+import psycopg2
 
 root = Tk()
+
+def get_data(name,color,age):
+    conn = psycopg2.connect(dbname="postgres", user="postgres", password="2349", host="localhost", port="5432")
+    cur = conn.cursor()
+    query = '''INSERT INTO demo (Name, Color, Age) VALUES (%s,%s,%s);'''
+    cur.execute(query,(name,color,age))
+    print("Sheep information added")
+    conn.commit()
+    conn.close()
 
 canvas = Canvas(root,height=480,width=900) # Canvas is rectangle space that is for complex elements
 canvas.pack()
@@ -27,7 +37,7 @@ age.grid(row=3, column=0)
 entry_age = Entry(frame)
 entry_age.grid(row=3,column=1)
 
-button = Button(frame,text="Submit Sheep")
+button = Button(frame,text="Submit Sheep",command=lambda:get_data(entry_name.get(),entry_color.get(),entry_age.get()))
 button.grid(row=4,column=1)
 
 root.mainloop()
